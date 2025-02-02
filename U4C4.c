5167df16153ba165c3ba7b0183d6b8898142d2d2
Kleber Marcal <kleber.sm@gmail.com>
2025-02-02 19:05:11 -0300

    Adiciona comentários

--- U4C4.c
+++ U4C4.c
@@ -11,37 +11,33 @@ static alarm_id_t dec_alm;
 
 
 void init_gpio() {
-    // uint32_t pinMask = 0x00 || 0x01 << BUTTON_A || 0x01 << BUTTON_B; 
-    // gpio_init_mask(pinMask);
-    // gpio_set_dir_masked(pinMask, GPIO_IN);
-    
-    // gpio_pull_up(BUTTON_A);
-    // gpio_pull_up(BUTTON_B);
-
+    // Inicializa o Botão A como entrada em PULL UP
     gpio_init(BUTTON_A);
-    gpio_set_dir(BUTTON_A, GPIO_IN); // Configura o pino como entrada
+    gpio_set_dir(BUTTON_A, GPIO_IN);
     gpio_pull_up(BUTTON_A);
 
+    // Inicializa o Botão B como entrada em PULL UP
     gpio_init(BUTTON_B);
     gpio_set_dir(BUTTON_B, GPIO_IN); // Configura o pino como entrada
     gpio_pull_up(BUTTON_B);
 
+    // Inicializa o pino do led vermelho como saída
     gpio_init(LED_R);
     gpio_set_dir(LED_R, GPIO_OUT);
 
-    // gpio_init(12);
-    // gpio_set_dir(12, GPIO_OUT);
-
+    // Habilita interrupções pros botões A e B
     gpio_set_irq_enabled_with_callback(BUTTON_A, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &gpio_callback);
     gpio_set_irq_enabled_with_callback(BUTTON_B, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &gpio_callback);
 }
 
+// Incrementa o valor de i
 int64_t increment(alarm_id_t id, void *user_data) {
     i++;
     if(i > 9) i = 0;
     return 0;
 }
 
+// Decrementa o valor de i
 int64_t decrement(alarm_id_t id, void *user_data) {
     i--;
     if(i < 0) i = 9;
@@ -49,41 +45,42 @@ int64_t decrement(alarm_id_t id, void *user_data) {
 }
 
 static void gpio_callback(uint gpio, uint32_t events) {
+    // Se o Evento aconteceu no Botão A
     if(gpio == BUTTON_A){
-        if(events & 0x4)        //FALL EDGE
-            dec_alm = add_alarm_in_ms(DEBOUNCE_MS, increment, NULL, false);
-        else if(events & 0x8)   // RISE EDGE
-            cancel_alarm(dec_alm); 
+        // Cancela o alarme de incremento, caso exista algum
+        cancel_alarm(inc_alm); 
+        // Se o Evento foi do tipo FALL EDGE (Borda de descida)
+        if(events & 0x4)
+            inc_alm = add_alarm_in_ms(DEBOUNCE_MS, increment, NULL, false);
     }
 
+    // Se o evento aconteceu no Botão B
     else if (gpio == BUTTON_B){
-        if(events & 0x4) 
-            inc_alm = add_alarm_in_ms(DEBOUNCE_MS, decrement, NULL, false);
+        // Cancela o alarme de decremento, caso exista algum
+        cancel_alarm(dec_alm);
+        // Se o Evento foi do tipo FALL EDGE (Borda de descida)
+        if(events & 0x4)
+            dec_alm = add_alarm_in_ms(DEBOUNCE_MS, decrement, NULL, false);
 
-        else if(events & 0x8)   // RISE EDGE
-            cancel_alarm(inc_alm);
     }
-}
-
-bool repeat_callback(struct repeating_timer *t) {
-
 }
 
 int main()
 {
     stdio_init_all();
 
+    // Inicializa os pinos de IO
     init_gpio();
+
+    // Inicializa a matriz de leds
     initNeoPixel();
+
+    // Cria uma variável para ficar alternando o status do led vermelho
     bool led = true;
 
-    // struct repeating_timer timer;
-    // add_repeating_timer_ms(1000, repeat_callback, NULL, &timer);
-
     while (true) {
-        // uint32_t start_ms = to_ms_since_boot(get_absolute_time());
-        // printf("Tempo absoluto: %d\n", start_ms);
-
+        
+        // Renderiza o caractere de acordo com o número armazenado em i
         switch(i){
             case 0:
                 renderMatrix(CHAR_0);
@@ -118,8 +115,11 @@ int main()
             default:
                 break;
         }
+
+        // Insere um delay de 100ms
         sleep_ms(100);
 
+        // Alterna o status do led vermelho
         led = !led;
         gpio_put(LED_R, led);
     }
